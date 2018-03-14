5bc8f799f302a4849744e5edfaa5cd53667e7bd3
Luke Bacon <luke@equivalentideas.com>
2018-03-15 09:07:27 +1100

    Toggle database config on environment

--- database_config.rb
+++ database_config.rb
@@ -1,7 +1,19 @@
-@database_connection = ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  host: 'localhost',
-  username: 'westconnex_m4east_aqm',
-  password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
-  database: 'westconnex_m4east_aqm'
+def environment
+  ENV['SCRIPT_ENV'] == 'production' ?  :production : :development
+end
+
+DATABASE_SETTINGS = {
+  development: {
+    host: 'localhost',
+    username: 'westconnex_m4east_aqm',
+    password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
+    database: 'westconnex_m4east_aqm_development'
+  },
+  production: {
+    url: ENV['DATABASE_URL']
+  }
+}.freeze
+
+ActiveRecord::Base.establish_connection(
+  { adapter: 'postgresql' }.merge(DATABASE_SETTINGS[environment])
 )
