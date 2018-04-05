09899d3acad8cd5d13fb6490ac1866b2ee7cfdc7
Henare Degan <henare.degan@gmail.com>
2018-04-05 14:54:28 +1000

    Extract database connection into it's own file
    This allows us to connect to the database without setting up the model.
    We need this when we're first creating the database structure.

--- database.rb
+++ database.rb
@@ -1,31 +1,5 @@
 # frozen_string_literal: true
 
-require 'sequel'
-
-def database_config
-  if ENV['RACK_ENV'] == 'production'
-    ENV['DATABASE_URL']
-  elsif ENV['RACK_ENV'] == 'test' && ENV['TRAVIS'] == 'true'
-    { adapter: 'postgresql', database: 'travis_ci_test' }
-  elsif ENV['RACK_ENV'] == 'test'
-    {
-      adapter: 'postgresql',
-      host: 'localhost',
-      username: 'westconnex_m4east_aqm',
-      password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
-      database: 'westconnex_m4east_aqm_test'
-    }
-  else # development
-    {
-      adapter: 'postgresql',
-      host: 'localhost',
-      username: 'westconnex_m4east_aqm',
-      password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
-      database: 'westconnex_m4east_aqm_development'
-    }
-  end
-end
-
-DB = Sequel.connect(database_config)
+require './db/connection'
 
 class AqmRecord < Sequel::Model; end
