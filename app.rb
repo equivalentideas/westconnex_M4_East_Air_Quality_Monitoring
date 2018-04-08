f483ffca9560061a8406daf4000be581a6ea8067
Henare Degan <henare.degan@gmail.com>
2018-04-08 15:27:52 +1000

    Make the name of the CSV download file more useful
    Includes the date and time the file was downloaded in the filename.
    
    The issue suggested, 'A file name like
    201803261243_m4east_air_quality_monitors.csv might be good?' but I
    thought that the jumble of numbers at the start of the filename might be
    confusing. Instead I've included separators that I hope make it clearer.
    
    Fixes #21

--- app.rb
+++ app.rb
@@ -9,9 +9,14 @@ get '/' do
   'Hello world!'
 end
 
+# TODO: Move this to a helper
+def csv_filename
+  Time.now.strftime('%F_%H%M') + '_m4east_air_quality_monitors.csv'
+end
+
 get '/csv' do
   content_type 'application/csv'
-  attachment 'aqm_records.csv'
+  attachment csv_filename
   CSV.generate do |csv|
     csv << AqmRecord.columns # Add header row
     AqmRecord.all.each do |record|
