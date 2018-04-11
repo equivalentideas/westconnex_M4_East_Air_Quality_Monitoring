268057188b9a07e12c27272224891df362e39de8
Henare Degan <henare.degan@gmail.com>
2018-04-11 12:26:42 +1000

    Use percent notation for array of symbols

--- db/migrations/004_correct_nil_measurement_values.rb
+++ db/migrations/004_correct_nil_measurement_values.rb
@@ -2,17 +2,17 @@
 
 Sequel.migration do
   up do
-    [
-      :latest_reading_recorded_at,
-      :pm2_5_concentration_ug_per_m3,
-      :pm10_concentration_ug_per_m3,
-      :co_concentration_ppm,
-      :no2_concentration_ppm,
-      :differential_temperature_lower_deg_c,
-      :differential_temperature_upper_deg_c,
-      :wind_speed_metres_per_second,
-      :wind_direction_deg_true_north,
-      :sigma_deg_true_north
+    %i[
+      latest_reading_recorded_at
+      pm2_5_concentration_ug_per_m3
+      pm10_concentration_ug_per_m3
+      co_concentration_ppm
+      no2_concentration_ppm
+      differential_temperature_lower_deg_c
+      differential_temperature_upper_deg_c
+      wind_speed_metres_per_second
+      wind_direction_deg_true_north
+      sigma_deg_true_north
     ].each do |field|
       from(:aqm_records).where(field => '-').update(field => nil)
     end
