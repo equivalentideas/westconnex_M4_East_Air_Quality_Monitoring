e8ec7071bf15c758bf8d412a824420d4527ad2ab
Henare Degan <henare.degan@gmail.com>
2018-04-12 10:26:11 +1000

    Show some statistics
    * This is a combination of 9 commits.
    * This is the 1st commit message:
    
    Show some statistics
    
    * This is the commit message #2:
    
    Show some statistics on the home page
    
    * This is the commit message #3:
    
    Add some table styling so the tables are readable
    
    * This is the commit message #4:
    
    Round averages displayed on home page
    
    We're using the same rounding as appears on the source site.
    
    * This is the commit message #5:
    
    Make rubocop happy
    
    Autocorrect everything we can and disable a cop inline that was
    incorrectly being triggered.
    
    * This is the commit message #6:
    
    Use Sequel's #map which makes this more concise
    
    * This is the commit message #7:
    
    Include readings of zero in our average calculation
    
    A reading of zero is potentially valid, unlike negative readings, so it
    should be included in the average calculation.
    
    * This is the commit message #8:
    
    Remove unnecessary disabling of Style/NumericPredicate cop
    
    It's now included in our main Rubocop config file.
    
    * This is the commit message #9:
    
    Move statistics page

--- app.rb
+++ app.rb
@@ -12,6 +12,28 @@ get '/' do
   erb :index
 end
 
+get '/statistics' do
+  sites = AqmRecord.select(:location_name).distinct.map(:location_name)
+  @statistics = sites.map do |location_name|
+    records = AqmRecord.where(location_name: location_name).order(:scraped_at)
+    {
+      location_name: location_name,
+      first_record: records.first.scraped_at,
+      record_count: records.count,
+      avg_pm2_5_concentration_ug_per_m3: records.where { pm2_5_concentration_ug_per_m3 >= 0 }.avg(:pm2_5_concentration_ug_per_m3).round(1),
+      negatives_pm2_5_concentration_ug_per_m3: records.where { pm2_5_concentration_ug_per_m3 < 0 }.count,
+      avg_pm10_concentration_ug_per_m3: records.where { pm10_concentration_ug_per_m3 >= 0 }.avg(:pm10_concentration_ug_per_m3).round(1),
+      negatives_pm10_concentration_ug_per_m3: records.where { pm10_concentration_ug_per_m3 < 0 }.count,
+      avg_co_concentration_ppm: records.where { co_concentration_ppm >= 0 }.avg(:co_concentration_ppm).round(2),
+      negatives_co_concentration_ppm: records.where { co_concentration_ppm < 0 }.count,
+      avg_no2_concentration_ppm: records.where { no2_concentration_ppm >= 0 }.avg(:no2_concentration_ppm).round(3),
+      negatives_no2_concentration_ppm: records.where { no2_concentration_ppm < 0 }.count
+    }
+  end
+
+  erb :statistics
+end
+
 # TODO: Move this to a helper
 def csv_filename
   Time.now.strftime('%F_%H%M') + '_m4east_air_quality_monitors.csv'
