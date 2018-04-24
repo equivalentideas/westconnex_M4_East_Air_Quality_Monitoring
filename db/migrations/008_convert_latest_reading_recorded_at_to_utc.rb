81e2f67a91ee45f8249664ba2f828f7012b8a291
Luke Bacon <luke@equivalentideas.com>
2018-04-24 17:18:01 +1000

    Convert existing latest_reading_recorded_at to UTC
    This is the format we want these in, see last commit 42e271a.
    
    This migration goes both ways thanks to the
    original_reading_datetime_string column, which has previously been
    populated with the existing latest_reading_recorded_at (see migration 007).
    
    Issue: https://github.com/equivalentideas/westconnex_M4_East_Air_Quality_Monitoring/issues/32

---
+++ db/migrations/008_convert_latest_reading_recorded_at_to_utc.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/scraper'
+
+Sequel.migration do
+  up do
+    from(:aqm_records).all.each do |record|
+      next unless record[:latest_reading_recorded_at]
+      converted_time = Scraper.new.convert_time(record[:latest_reading_recorded_at])
+
+      record.update(
+        latest_reading_recorded_at: converted_time.to_s
+      )
+
+      from(:aqm_records).where(id: record[:id]).update(record)
+    end
+  end
+
+  down do
+    from(:aqm_records).all.each do |record|
+      record.update(
+        latest_reading_recorded_at: record[:original_reading_datetime_string]
+      )
+
+      from(:aqm_records).where(id: record[:id]).update(record)
+    end
+  end
+end
