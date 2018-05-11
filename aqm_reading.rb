4a160a9d4b8de0dd37297e4b18f86ca4f22b7b12
Henare Degan <henare.degan@gmail.com>
2018-05-11 16:43:18 +1000

    Add more parameters that we're going to need to the constructor
    These two bits of data aren't in the HTML so we're going to need to
    specify them from ourside the class.

--- aqm_reading.rb
+++ aqm_reading.rb
@@ -4,15 +4,20 @@ require 'nokogiri'
 
 # A reading comprising several measurements from an air quality monitoring site
 class AqmReading
-  attr_reader :raw_data
+  attr_reader :raw_data, :location_name, :scraped_at
 
   # @param raw_data [String] HTML of page containing air quality measurements
-  def initialize(raw_data:)
+  def initialize(raw_data:, location_name:, scraped_at:)
     @raw_data = Nokogiri::HTML(raw_data)
+    @location_name = location_name
+    @scraped_at = scraped_at
   end
 
   # Outputs a hash of all the data for this reading, suitable for saving to the database
   def data
-    {}
+    {
+      location_name: location_name,
+      scraped_at: scraped_at
+    }
   end
 end
