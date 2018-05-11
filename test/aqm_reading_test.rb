8564f13066c526e742c440ee11dc7fc1c61965f4
Henare Degan <henare.degan@gmail.com>
2018-05-11 16:28:47 +1000

    Use a more descriptive variable name

--- test/aqm_reading_test.rb
+++ test/aqm_reading_test.rb
@@ -5,8 +5,8 @@ require_relative '../aqm_reading'
 
 describe AqmReading do
   subject do
-    fixture_file = File.read(File.join(File.dirname(__FILE__), 'fixtures/haberfield.html'))
-    AqmReading.new(raw_data: fixture_file)
+    haberfield_html = File.read(File.join(File.dirname(__FILE__), 'fixtures/haberfield.html'))
+    AqmReading.new(raw_data: haberfield_html)
   end
 
   it 'reads the file' do
