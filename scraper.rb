b0c19ab20aeb444003ec409e3b00945ff5fe7dcb
Henare Degan <henare.degan@gmail.com>
2018-05-17 16:53:32 +1000

    Remove redundant lib/scraper.rb file
    We can move all this procedural code back into the main scraper
    executable now that it's really simple.

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative 'lib/scraper'
+require_relative 'database'
+require_relative 'lib/aqm_sites'
+require_relative 'lib/aqm_reading'
+
+AQM_SITES.each do |site|
+  aqm_reading = AqmReading.new(
+    location_name: site.location_name,
+    raw_data: site.json,
+    scraped_at: Time.now
+  )
+  AqmRecord.create(aqm_reading.data)
+end
