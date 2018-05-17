352f16318514673e3dd4a0d7fa1efef3504f1aaa
Henare Degan <henare.degan@gmail.com>
2018-05-17 13:35:41 +1000

    Remove manipulation of latest_reading_recorded_at from database model
    As per the previous commit 4431974 I think our other class should be
    responsible for all data manipulation, especially something like this
    that is so tightly coupled to the input data.

--- database.rb
+++ database.rb
@@ -3,15 +3,4 @@
 require './db/connection'
 
 # Database model for scraped air quality measurement records
-class AqmRecord < Sequel::Model
-  # Store raw value as a backup and convert value to UTC for storage in the database
-  def latest_reading_recorded_at=(date_and_time)
-    if date_and_time.nil? || date_and_time.strip.empty?
-      @values[:latest_reading_recorded_at_raw] = nil
-      @values[:latest_reading_recorded_at] = nil
-    else
-      @values[:latest_reading_recorded_at_raw] = date_and_time
-      @values[:latest_reading_recorded_at] = Time.parse(date_and_time.gsub(/\b\S*$/, '+0000')) - (60 * 60 * 10)
-    end
-  end
-end
+class AqmRecord < Sequel::Model; end
