b66de2eb1e8f5a1dbbcc23e160159ae54fae4de4
Henare Degan <henare.degan@gmail.com>
2018-05-17 11:42:54 +1000

    Correctly specify timezone in Time object creation
    This wasn't affecting anything right now but we know from 9c1d5e7 that
    it can.

--- test/aqm_reading_test.rb
+++ test/aqm_reading_test.rb
@@ -61,7 +61,7 @@ describe AqmReading do
     describe 'when date and time is +10 time' do
       it 'converts it to UTC' do
         subject.stub :latest_reading_recorded_at_raw, '24 April 2018 at 3:30:00 pm AEST' do
-          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, 0)
+          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, '+00:00')
         end
       end
     end
@@ -69,7 +69,7 @@ describe AqmReading do
     describe 'when date and time is +10 time and incorrectly marked GMT' do
       it 'reads it as +10 time and converts it to UTC' do
         subject.stub :latest_reading_recorded_at_raw, 'April 24, 2018 3:30:00 PM GMT' do
-          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, 0)
+          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, '+00:00')
         end
       end
     end
@@ -77,14 +77,14 @@ describe AqmReading do
     describe 'when date and time is +10 time and incorrectly marked AEDT' do
       it 'reads it as +10 time and converts it to UTC' do
         subject.stub :latest_reading_recorded_at_raw, '24 April 2018 3:30:00 pm AEDT' do
-          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, 0)
+          subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 24, 5, 30, 0, '+00:00')
         end
       end
 
       describe 'without a padded day number' do
         it 'converts it successfully' do
           subject.stub :latest_reading_recorded_at_raw, 'April 7, 2018 1:00:00 AM GMT' do
-            subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 6, 15, 0, 0, 0)
+            subject.latest_reading_recorded_at.must_equal Time.new(2018, 4, 6, 15, 0, 0, '+00:00')
           end
         end
       end
