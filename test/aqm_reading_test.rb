7e4de59e68155530027204ce08370d465cb2554c
Henare Degan <henare.degan@gmail.com>
2018-05-17 10:54:45 +1000

    Get test file ready to add more tests
    We're going to put all tests in the Timecop block to make things easy
    for now so add a comment to that effect.
    
    Put the existing test in it's own describe block so we can keep things
    organised as we add more tests.

--- test/aqm_reading_test.rb
+++ test/aqm_reading_test.rb
@@ -4,27 +4,30 @@ require 'test_helper'
 require_relative '../lib/aqm_reading'
 
 describe AqmReading do
+  # All tests are wrapped in this Timecop block to make testing scraped_at easier
   Timecop.freeze do
     subject do
       haberfield_html = File.read(File.join(File.dirname(__FILE__), 'fixtures/haberfield.html'))
       AqmReading.new(raw_data: haberfield_html, location_name: 'Haberfield Public School AQM', scraped_at: Time.now)
     end
 
-    it 'should provide a hash of all the data' do
-      subject.data.must_equal({
-        location_name: 'Haberfield Public School AQM',
-        scraped_at: Time.now,
-        latest_reading_recorded_at: Time.new(2018, 5, 11, 15, 30, 0, 10),
-        pm2_5_concentration_ug_per_m3: 14,
-        pm10_concentration_ug_per_m3: 16,
-        co_concentration_ppm: 0,
-        no2_concentration_ppm: 0.007,
-        differential_temperature_lower_deg_c: 15.8,
-        differential_temperature_upper_deg_c: 15.7,
-        wind_speed_metres_per_second: 4.6,
-        wind_direction_deg_true_north: 266,
-        sigma_deg_true_north: 23.3
-      })
+    describe '#data' do
+      it 'should provide a hash of all the data' do
+        subject.data.must_equal({
+          location_name: 'Haberfield Public School AQM',
+          scraped_at: Time.now,
+          latest_reading_recorded_at: Time.new(2018, 5, 11, 15, 30, 0, 10),
+          pm2_5_concentration_ug_per_m3: 14,
+          pm10_concentration_ug_per_m3: 16,
+          co_concentration_ppm: 0,
+          no2_concentration_ppm: 0.007,
+          differential_temperature_lower_deg_c: 15.8,
+          differential_temperature_upper_deg_c: 15.7,
+          wind_speed_metres_per_second: 4.6,
+          wind_direction_deg_true_north: 266,
+          sigma_deg_true_north: 23.3
+        })
+      end
     end
   end
 end
