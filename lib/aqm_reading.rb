ee57252e33a08574217ae2a7e4501068ddb17a65
Henare Degan <henare.degan@gmail.com>
2018-05-17 13:08:08 +1000

    Cast measurements to Float from String
    We expect these measurements in numeric form so that's the right thing
    to do. We need to retain `nil`s though so they're saved in the database
    as NULL instead of 0.0.
    
    This is doing the simple thing first and is becoming a good refactoring
    target.

--- lib/aqm_reading.rb
+++ lib/aqm_reading.rb
@@ -47,6 +47,7 @@ class AqmReading
   def measurements
     key_rows = raw_data.search('tbody th').map { |th| tableize(th.text) }
     value_rows = raw_data.search('tbody td').map { |td| extract_value(td.text) }
+    value_rows.map! { |value| value.to_f unless value.nil? }
     measurements = key_rows.zip(value_rows).to_h
   end
 
