2992f40070c6534183f2cee1c4abc6d74c91e457
Henare Degan <henare.degan@gmail.com>
2018-05-23 14:33:37 +1000

    Extract environment method
    This extracts the logic for determining the current environment from the
    database_config method. It makes that method simpler and these separate
    methods are now easier to read.

--- db/connection.rb
+++ db/connection.rb
@@ -2,31 +2,40 @@
 
 require 'sequel'
 
-def database_config
+def environment
   if ENV['RACK_ENV'] == 'production'
-    ENV['DATABASE_URL']
+    :production
   elsif ENV['RACK_ENV'] == 'test' && ENV['TRAVIS'] == 'true'
-    { adapter: 'postgresql', database: 'travis_ci_test' }
+    :travis
   elsif ENV['RACK_ENV'] == 'test'
-    {
+    :test
+  else
+    :development
+  end
+end
+
+def database_config
+  {
+    production: ENV['DATABASE_URL'],
+    travis: { adapter: 'postgresql', database: 'travis_ci_test' },
+    test: {
       adapter: 'postgresql',
       host: 'localhost',
       username: 'westconnex_m4east_aqm',
       password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
       database: 'westconnex_m4east_aqm_test'
-    }
-  else # development
-    {
+    },
+    development: {
       adapter: 'postgresql',
       host: 'localhost',
       username: 'westconnex_m4east_aqm',
       password: ENV['DEVELOPMENT_DATABASE_PASSWORD'],
       database: 'westconnex_m4east_aqm_development'
     }
-  end
+  }
 end
 
 Sequel.database_timezone = :utc
 Sequel.application_timezone = :local
 
-DB = Sequel.connect(database_config)
+DB = Sequel.connect(database_config[environment])
