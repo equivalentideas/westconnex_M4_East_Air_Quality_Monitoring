f75eecd4a9549b59e5e8882907aa0d0159341037
Henare Degan <henare.degan@gmail.com>
2019-02-07 16:30:37 +1100

    Allow access to better_errors from Docker private IP ranges
    This is because I'm using Docker locally for development

--- config/app_config.rb
+++ config/app_config.rb
@@ -5,6 +5,7 @@ require 'better_errors' unless %w[production test].include? ENV['RACK_ENV']
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = __dir__
+  BetterErrors::Middleware.allow_ip! '172.16.0.0/12'
 end
 
 require './db/connection'
